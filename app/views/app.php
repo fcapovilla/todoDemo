d367640001cc652ad69043cd1a34e5baf08305db
frederick <frederick@localhost.localdomain>
2014-01-26 14:31:20 -0500

    Ajout d'une vue de base

--- app/views/app.php
+++ app/views/app.php
@@ -3,14 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Todos</title>
+</head>
+<body>
+	<h1>Todos</h1>
+	<div id="content">
+	</div>
+
+	<script type="text/template" id="tmpl_todo_list">
+		<ul>
+			<li><button class="newTodo">Nouveau</button></li>
+		</ul>
+	</script>
+
 	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="js/lodash.min.js"></script>
 	<script type="text/javascript" src="js/backbone-min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
-</head>
-<body>
-	<h1>Todos</h1>
-	<div class="content">
-	</div>
 </body>
 </html>
--- public/js/app.js
+++ public/js/app.js
@@ -10,10 +10,23 @@ TodoApp.Collection.Todo = Backbone.Collection.extend({
 	url: '/todo'
 });
 
+TodoApp.View.TodoList = Backbone.View.extend({
+	el: '#content',
+	template: _.template($('#tmpl_todo_list').html()),
+
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html( this.template() );
+		return this;
+	}
+});
+
 
 $(function() {
 	TodoApp.todos = new TodoApp.Collection.Todo();
+	TodoApp.todoList = new TodoApp.View.TodoList({collection: TodoApp.todos});
 	TodoApp.todos.fetch();
-
-	console.log(TodoApp.todos);
 });
